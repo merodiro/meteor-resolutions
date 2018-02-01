f46b4704179948ce646e840d4f00d4fe84c1f943
Amr A.Mohammed <merodiro@gmail.com>
2018-02-01 03:49:47 +0200

    setup apollo engine

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,9 @@
+import { Meteor } from 'meteor/meteor';
 import { createApolloServer } from 'meteor/apollo'
+import { Engine } from 'apollo-engine'
 import { makeExecutableSchema } from 'graphql-tools'
+
+const PORT = process.env.PORT || 3000;
 
 const typeDefs = `
 type Query {
@@ -19,7 +23,27 @@ const schema = makeExecutableSchema({
     resolvers,
 })
 
+// Initialize Apollo Engine
+const engine = new Engine({
+    engineConfig: {
+        apiKey: 'service:merodiro-4894:G5lFcAjcRJ3u4wuOBxax3A',
+        logging: {
+            level: 'DEBUG', // DEBUG, INFO, WARN or ERROR
+        },
+    },
+    graphqlPort: PORT,
+    endpoint: '/graphql',
+})
 
-createApolloServer({
-    schema
+engine.start()
+createApolloServer(req => ({
+    schema,
+    context: {},
+    tracing: true,
+    cacheControl: true,
+}), {
+    configServer: graphQLServer => {
+        graphQLServer.use(engine.expressMiddleware());
+        // Any other config server stuff
+    },
 })
