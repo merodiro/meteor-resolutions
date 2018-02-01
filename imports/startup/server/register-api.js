c1ae5c89920ab828cc2784ba9dce1e030726ca01
Amr A.Mohammed <merodiro@gmail.com>
2018-02-01 08:58:51 +0200

    adding resolutions schema

---
+++ imports/startup/server/register-api.js
@@ -0,0 +1,56 @@
+import { Meteor } from 'meteor/meteor';
+import { createApolloServer } from 'meteor/apollo'
+import { Engine } from 'apollo-engine'
+import { makeExecutableSchema } from 'graphql-tools'
+
+import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql'
+
+const PORT = process.env.PORT || 3000;
+
+const testSchema = `
+type Query {
+    hi: String
+}
+`
+
+const typeDefs = [
+    testSchema,
+    ResolutionsSchema
+]
+const resolvers = {
+    Query: {
+        hi() {
+            return "Hello World"
+        }
+    }
+}
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
+
+// Initialize Apollo Engine
+const engine = new Engine({
+    engineConfig: {
+        apiKey: 'service:merodiro-4894:G5lFcAjcRJ3u4wuOBxax3A',
+        logging: {
+            level: 'DEBUG', // DEBUG, INFO, WARN or ERROR
+        },
+    },
+    graphqlPort: PORT,
+    endpoint: '/graphql',
+})
+
+engine.start()
+createApolloServer(req => ({
+    schema,
+    context: {},
+    tracing: true,
+    cacheControl: true,
+}), {
+    configServer: graphQLServer => {
+        graphQLServer.use(engine.expressMiddleware());
+        // Any other config server stuff
+    },
+})
